d9560869e8d2f4977baae32a190ce399233ef08d
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-10 17:17:18 +0100

    Forgot a line

--- main.sh
+++ main.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
+
 source func_require_root.sh
 echo -e "\e[92m[dnf] Updating system\e[39m"
 dnf update -y 1>/dev/null
 
+source init_banner.sh
 source init_users.sh
 source init_lamp.sh
 source init_fail2ban.sh
 source init_wordpress.sh
 source init_vsftpd.sh
+
 set +e
