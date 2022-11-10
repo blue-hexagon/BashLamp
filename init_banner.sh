dad18ff4028f53c5ada5320f8109bdb722397164
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-10 17:36:11 +0100

    Add a colorful prompt

--- init_banner.sh
+++ init_banner.sh
@@ -9,7 +9,8 @@ if [[ $? -eq 1 ]]; then
 	gem install lolcat
 	rm -rf lolcat-master
 	rm -rf master.zip
-	echo '#CONF_BANNER_SET=TRUE' >> ~/.bashrc
 	echo 'figlet -w 120 -f "/usr/share/figlet/fonts/3d.flf" "Ditzels LAMP" | lolcat' >> ~/.bashrc
 	echo 'echo " Ditzels LAMP Server - Store Patter Betyder Undskyld" | lolcat' >> ~/.bashrc
+	echo 'PS1='\[\e[0;38;5;46m\][\[\e[0;38;5;214m\]\u\[\e[0m\]@\[\e[0;38;5;205m\]\h \[\e[0;38;5;226m\]\w\[\e[0;38;5;46m\]]\[\e[0;1;38;5;214m\]\$\[\e[0m\]'' >> ~/.bashrc
+	echo '#CONF_BANNER_SET=TRUE' >> ~/.bashrc
 fi
