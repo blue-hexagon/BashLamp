6960a712a2ede4f5e91374e5c4ccbdb3416d92cf
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-10 13:35:56 +0100

    Add SSL configuration to the ftp setup script

--- init_vsftpd.sh
+++ init_vsftpd.sh
@@ -1,14 +1,15 @@
 #!/bin/bash    
 source func_colors.sh
 
-
 print_yellow "For resources: https://techviewleo.com/install-and-configure-vsftpd-ftp-server-on-rocky-linux/#comments"
 
 dnf install -y vsftpd openssl
 systemctl enable --now vsftpd
 
 firewall-cmd --zone=public --permanent --add-port=21/tcp
+firewall-cmd --zone=public --permanent --add-port=30000-31000/tcp
 firewall-cmd --zone=public --permanent --add-service=ftp
+
 firewall-cmd --reload
 
 adduser vsftpduser
@@ -30,5 +31,11 @@ echo "pasv_min_port=30000" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=31000" >> /etc/vsftpd/vsftpd.conf
 echo "userlist_file=/etc/vsftpd/user_list" >> /etc/vsftpd/vsftpd.conf
 echo "userlist_deny=NO" >> /etc/vsftpd/vsftpd.conf
+
+sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/vsftpd.pem -out /etc/vsftpd/vsftpd.pem
+
+echo "rsa_cert_file=/etc/vsftpd/vsftpd.pem" >> /etc/vsftpd/vsftpd.conf
+echo "rsa_private_key_file=/etc/vsftpd.pem" >> /etc/vsftpd/vsftpd.conf
+echo "ssl_enable=ON" >> /etc/vsftpd/vsftpd.conf
 
 sudo systemctl restart vsftpd
