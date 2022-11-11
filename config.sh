f32880c299abbb27f407bf303842e562bab5db00
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-11 20:32:01 +0100

    Missed a letter in a variable name

--- config.sh
+++ config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+dnf install -y dialog
 form_new_users="$(dialog --inputbox "Enter a list of users to create (space delimited)" 		   20 70 "" 3>&1 1>&2 2>&3)"
 form_ftp_users="$(dialog --inputbox "Add users allowed to ftp into the server (space delimited)"   20 70 "" 3>&1 1>&2 2>&3)"
 form_users_password="$(dialog --inputbox "Add a default password for the new users"  		   20 70 "" 3>&1 1>&2 2>&3)"
 
-form_worpress_db="$(dialog --form "Enter wp database details (no spaces!)"   20 70  14    \
+form_wordpress_db="$(dialog --form "Enter wp database details (no spaces!)"   20 70  14    \
              "Wordpress Database Name: "         1 1 "wordpress" 1  35  35  0 \
              "Wordpress Database Password: "     2 1 "password"  2  35  35  0 \
              "Wordpress Database User: "         3 1 "wpuser"    3  35  35  0 \
