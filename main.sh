1fbb38c772884c67f37d1728face44316bdaac15
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-11 09:04:17 +0100

    Add strictmode (set -e) as an option when running the script

--- main.sh
+++ main.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-set -e
+
+read -p "Run strict install mode with: set -e ? (y/n): " strictmode
+
+if [[ ${strictmode,,} == "y" || ${strictmode,,} == "yes" || ${strictmode} == "" ]]
+then
+        set -e
+fi
 
 source func_require_root.sh
+
 echo -e "\e[92m[dnf] Updating system\e[39m"
 dnf update -y
 
 echo -e "\e[92m[dnf] Installing: epel-release\e[39m"
 echo -e "\e[92m[dnf] Installing: vim, tmux, byobu and wget\e[39m"
 dnf install -y epel-release
-dnf install -y vim tmux wget byobu 1>/dev/null
+dnf install -y vim tmux wget byobu
 
 source init_alias.sh
 source init_banner.sh
@@ -18,4 +25,7 @@ source init_fail2ban.sh
 source init_wordpress.sh
 source init_vsftpd.sh
 
-set +e
+if [[ ${strictmode,,} == "y" || ${strictmode,,} == "yes" || ${strictmode} == "" ]]
+then
+        set +e
+fi
