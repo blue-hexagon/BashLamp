10e1ae6616339f4953d23cd7de7b5f2e4e1ad3fd
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-11 18:07:50 +0100

    Put wordpress database config into the config file

--- config.sh
+++ config.sh
@@ -2,3 +2,6 @@
 new_users=( tobi801j tobi802j stef965j mort46n1 )
 users_password="Kode1234!"
 ftp_users=( tobi801j )
+wordpress_db_name="wordpress"
+wordpress_db_password="password"
+wordpress_db_user="wpuser"
