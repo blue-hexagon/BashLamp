2d70d59504ade48e0d8ecd46252d762e5f73c01a
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-11-12 11:51:05 +0100

    Add a menu for interfacing with the program

--- init_fail2ban.sh
+++ init_fail2ban.sh
@@ -6,10 +6,8 @@ if [[ $? -eq 1 ]]; then
 	dnf install -y epel-release > /dev/null 2>&1
 	print_green "[dnf] installed epel-release"
 
-
 	dnf install -y fail2ban > /dev/null 2>&1
 	print_green "[dnf] installed fail2ban"
-
 
 	systemctl enable --now fail2ban > /dev/null 2>&1
 	print_green "[systemctl] enabled and started fail2ban"
@@ -25,10 +23,4 @@ if [[ $? -eq 1 ]]; then
 	print_green "[sed] Changed backend from auto to systemd in /etc/fail2ban/jail.local"
 	print_green "[script] executed successfully"
 	echo "FAIL2BAN_INSTALLED" >> ${rcfile}
-fi
-
-read -p "Open /etc/fail2ban/jail.local to modify the config? (y/n): " choice
-
-if [[ ${choice,,} == "y" || ${choice,,} == "yes" || ${choice} == "" ]]; then
-      vim /etc/fail2ban/jail.local
 fi
